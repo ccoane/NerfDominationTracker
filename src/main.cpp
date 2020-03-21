5296b148f1d435adfbd3686bb9b2f07f69b15f18
CodyECSL <ccoane@gmail.com>
2020-03-21 15:25:44 -0500

    Big update to UI

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,13 @@
 #include <MillisTimer.h>
 #include <string>
 #include <pogchamp.h>
+#include <triangle.h>
+#include <GothamLight_7.h>
+#include <GothamLight_9.h>
+#include <GothamLight_12.h>
+#include <GothamBook_7.h>
+#include <GothamBook_9.h>
+#include <GothamBook_12.h>
 
 #ifndef TFT_DISPOFF
 #define TFT_DISPOFF 0x28
@@ -38,7 +45,7 @@
 // #define SCREEN_HEIGHT         120
 // #define SCREEN_WIDTH          230
 
-#define POINTS_TO_WIN            20
+int pointsToWin = 20;
 
 TFT_eSPI tft = TFT_eSPI(SCREEN_HEIGHT, SCREEN_WIDTH);
 Button2 btn1(BUTTON_1);
@@ -48,17 +55,23 @@ char buff[512];
 int vref = 1100;
 int btnCick = false;
 
+HTTPClient http;
+
 const char* ssid = WIFI_SSID;
 const char* password =  WIFI_PASSWD;
 
-String baseUrl = "http://nerf-data-api-dfw.herokuapp.com";
-String urlStatus = "/koth/status";
+String baseUrl = "http://nerf-data-api-dfw.herokuapp.com/koth";
+String urlStatus = "/status";
+String urlStopTimers = "/stopTimers";
 
 struct JsonTeamStruct {
   const char* teamName = ""; // "Red"
   bool isActive = false; // false
   int elapsedTimeInSeconds = 0; // 0
 }jsonTeamStruct;
+
+String elapsedGameTime = "";
+const char* ElapsedGameTimeFormatted = "";
 
 #define sizeOfTeamsAllowed 4
 JsonTeamStruct teams[sizeOfTeamsAllowed];
@@ -154,9 +167,7 @@ void DrawTextCentered (String text) {
 
 void UpdateStatus(MillisTimer &mt) {
   if (WiFi.status() == WL_CONNECTED) { //Check the current connection status
- 
-    HTTPClient http;
- 
+  
     String fullUrl = baseUrl + urlStatus;
     Serial.println(fullUrl);
     http.begin(fullUrl);
@@ -188,6 +199,7 @@ void SetTeamValuesFromJson (String jsonVal) {
   
   deserializeJson(doc, jsonVal);
 
+  ElapsedGameTimeFormatted = doc["ElapsedGameTimeFormatted"];
   JsonArray Teams = doc["Teams"];
   
   for (int i = 0 ; i < sizeOfTeamsAllowed; i++) {
@@ -197,18 +209,23 @@ void SetTeamValuesFromJson (String jsonVal) {
       teams[i].teamName = Team["teamName"];
       teams[i].isActive = Team["isActive"];
       teams[i].elapsedTimeInSeconds = Team["elapsedTimeInSeconds"];
-      // Serial.println(String(teams[i].teamName));
-      // Serial.println(String(teams[i].elapsedTimeInSeconds));
+      if (teams[i].elapsedTimeInSeconds >= pointsToWin) {
+        String fullUrl = baseUrl + urlStopTimers;
+        Serial.println(fullUrl);
+        http.begin(fullUrl);
+        int httpCode = http.GET();
+        Serial.println(httpCode);
+      }
     }
   }
 }
 
 void UpdateDisplay() {
   // Domination Name
-  tft.setTextDatum(TL_DATUM);
-  tft.setFreeFont(&FreeSansBoldOblique12pt7b);
-  tft.setTextColor(TFT_WHITE);
-  tft.drawString( "DOMINATION" , 0 , 0 );
+  // tft.setTextDatum(TL_DATUM);
+  // tft.setFreeFont(&FreeSansBoldOblique12pt7b);
+  // tft.setTextColor(TFT_WHITE);
+  // tft.drawString( "DOMINATION" , SCREEN_WIDTH *.05 , 0 );
 
   // Voltage
   double voltage = GetVoltage();
@@ -218,12 +235,29 @@ void UpdateDisplay() {
     tft.setTextColor(TFT_RED, TFT_BLACK);
   }
   String voltageText = String(voltage) + "v";
-  tft.setTextDatum(TR_DATUM);
+  tft.setTextDatum(TL_DATUM);
   tft.setFreeFont(&FreeSans9pt7b);
   tft.setTextSize(1);
   int padding = tft.textWidth("99.9v", GFXFF); // get the width of the text in pixels
   tft.setTextPadding(padding);
-  tft.drawString( voltageText , SCREEN_WIDTH , 0 );
+  tft.drawString( voltageText , SCREEN_WIDTH * .05 , SCREEN_HEIGHT * .05 );
+
+  // Elapsed Game Time
+  tft.setTextDatum(TR_DATUM);
+  tft.setFreeFont(&FreeSans9pt7b);
+  tft.setTextSize(1);
+  padding = tft.textWidth("00:00", GFXFF); // get the width of the text in pixels
+  tft.setTextPadding(padding);
+  tft.drawString( String(ElapsedGameTimeFormatted) , SCREEN_WIDTH , SCREEN_HEIGHT * .05 );
+
+  // Signal Strength
+  tft.setTextDatum(TC_DATUM);
+  tft.setFreeFont(&FreeSans9pt7b);
+  tft.setTextSize(1);
+  padding = tft.textWidth("-99dBm", GFXFF); // get the width of the text in pixels
+  tft.setTextPadding(padding);
+  String signalStrength = String(WiFi.RSSI()) + "dBm";
+  tft.drawString( signalStrength , SCREEN_WIDTH / 2 , SCREEN_HEIGHT * .05 );
 
   // Team Info
   for (int i =0 ; i < 2 ; i++){
@@ -231,23 +265,28 @@ void UpdateDisplay() {
     if (teamName.length() > 0) {
       
       // Progress Bar Background
-      tft.fillRect( SCREEN_WIDTH *.05 , ( SCREEN_HEIGHT * .30 ) + (i * 32 ), SCREEN_WIDTH * .9, 30, GetTeamColor(teams[i].teamName, false));
+      tft.fillRect( SCREEN_WIDTH *.05 , ( SCREEN_HEIGHT * .25 ) + (i * 32 ), SCREEN_WIDTH * .9, 30, GetTeamColor(teams[i].teamName, false));
 
       // Progress Bar Begin/Mids/End Pieces
-      tft.fillRect( SCREEN_WIDTH *.05 , ( SCREEN_HEIGHT * .30 ) + (i * 32 ), SCREEN_WIDTH * .025, 30, GetTeamColor(teams[i].teamName, true));
-      tft.fillRect( SCREEN_WIDTH *.50 , ( SCREEN_HEIGHT * .30 ) + (i * 32 ), SCREEN_WIDTH * .025, 30, GetTeamColor(teams[i].teamName, true));
-      tft.fillRect( SCREEN_WIDTH *.80 , ( SCREEN_HEIGHT * .30 ) + (i * 32 ), SCREEN_WIDTH * .025, 30, GetTeamColor(teams[i].teamName, true));
-      tft.fillRect( SCREEN_WIDTH *.95 , ( SCREEN_HEIGHT * .30 ) + (i * 32 ), SCREEN_WIDTH * .025, 30, GetTeamColor(teams[i].teamName, true));
+      tft.fillRect( SCREEN_WIDTH * .05 , ( SCREEN_HEIGHT * .25 ) + (i * 32 ), SCREEN_WIDTH * .025, 30, GetTeamColor(teams[i].teamName, true));
+      tft.fillRect( SCREEN_WIDTH * .50 , ( SCREEN_HEIGHT * .25 ) + (i * 32 ), SCREEN_WIDTH * .025, 30, GetTeamColor(teams[i].teamName, true));
+      tft.fillRect( SCREEN_WIDTH * .80 , ( SCREEN_HEIGHT * .25 ) + (i * 32 ), SCREEN_WIDTH * .025, 30, GetTeamColor(teams[i].teamName, true));
+      tft.fillRect( SCREEN_WIDTH * .95 , ( SCREEN_HEIGHT * .25 ) + (i * 32 ), SCREEN_WIDTH * .025, 30, GetTeamColor(teams[i].teamName, true));
 
+      // Draw Control Point Markers
+      if (teams[i].isActive) {
+        tft.pushImage( ( SCREEN_WIDTH * .50 ) + 30, ( SCREEN_HEIGHT * .25 ) + (i * 32 ) + 2 , 25, 25, triangle, TFT_BLACK );
+      }
+      
       // Progress Bar
       double teamPoints = teams[i].elapsedTimeInSeconds; 
 
       double ProgressBarEndLocation = SCREEN_WIDTH *.05 + SCREEN_WIDTH * .025;          // 18.00
       double ProgressBarStartLocation = SCREEN_WIDTH *.50;                              // 120.00
       double ProgressBarTotalSize = ProgressBarStartLocation - ProgressBarEndLocation;  // 102.00
-      double ProgressBarStartLocationUpdate = ProgressBarStartLocation - (ProgressBarTotalSize * (teamPoints / POINTS_TO_WIN ));
+      double ProgressBarStartLocationUpdate = ProgressBarStartLocation - (ProgressBarTotalSize * (teamPoints / pointsToWin ));
       double ProgressBarStartLocationUpdateWidth = (ProgressBarStartLocationUpdate - ProgressBarStartLocation) * -1;
-      tft.fillRect( ProgressBarStartLocationUpdate , ( SCREEN_HEIGHT * .30 ) + ( i * 32 ), ProgressBarStartLocationUpdateWidth , 30 , GetTeamColor( teams[i].teamName, true ) );
+      tft.fillRect( ProgressBarStartLocationUpdate , ( SCREEN_HEIGHT * .25 ) + ( i * 32 ), ProgressBarStartLocationUpdateWidth , 30 , GetTeamColor( teams[i].teamName, true ) );
 
       // Number on top of Progress Bar
       tft.setTextDatum(BC_DATUM);
@@ -255,28 +294,28 @@ void UpdateDisplay() {
       tft.setTextColor(TFT_WHITE);
       padding = tft.textWidth("999", GFXFF); // get the width of the text in pixels
       tft.setTextPadding(padding);
-      tft.drawString( String( (int) teamPoints ) , ( SCREEN_WIDTH * .50 ) * .90 , ( SCREEN_HEIGHT * .50 ) + (i * 32 ) );
+      tft.drawString( String( (int) teamPoints ) , ( SCREEN_WIDTH * .50 ) * .90 , ( SCREEN_HEIGHT * .45 ) + (i * 32 ) );
 
       // Team Letter
       tft.setFreeFont(&FreeSansBoldOblique12pt7b);
       tft.setTextColor(TFT_WHITE);
       padding = tft.textWidth("R", GFXFF); // get the width of the text in pixels
       tft.setTextPadding(padding);
-      tft.drawString( String(teams[i].teamName[0]) , SCREEN_WIDTH  * .88 , ( SCREEN_HEIGHT * .50 ) + (i * 32 ) );
+      tft.drawString( String(teams[i].teamName[0]) , SCREEN_WIDTH  * .88 , ( SCREEN_HEIGHT * .45 ) + (i * 32 ) );
     }
   }
 
   // Game Mode Info Bottom Bar
   tft.setTextDatum(BC_DATUM);
-  // padding = tft.textWidth("300 Points to Win | Domination", GFXFF); // get the width of the text in pixels
-  padding = tft.textWidth("300 Points to Win", GFXFF); // get the width of the text in pixels
+  padding = tft.textWidth("999 Points to Win | Domination", GFXFF); // get the width of the text in pixels
+  // padding = tft.textWidth("999 Points to Win", GFXFF); // get the width of the text in pixels
   tft.setTextPadding(padding);
-  tft.setFreeFont(&FreeSans9pt7b);
-  tft.setTextSize(.50);
+  tft.setFreeFont(&GothamLight9pt7b);
   tft.setTextColor(TFT_WHITE);
-  // String bottomRowText = String(POINTS_TO_WIN) + " Points to Win | Domination";
-  String bottomRowText = String(POINTS_TO_WIN) + " Points to Win";
-  tft.drawString( bottomRowText , SCREEN_WIDTH *.5 , SCREEN_HEIGHT );
+  String bottomRowTextp1 = String(pointsToWin) + " Points to Win";
+  tft.drawString( bottomRowTextp1 , SCREEN_WIDTH *.5 , SCREEN_HEIGHT * .86 );
+  String bottomRowTextp2 = "Domination";
+  tft.drawString( bottomRowTextp2 , SCREEN_WIDTH *.5 , SCREEN_HEIGHT );
 }
 
 // tft.setTextColor(GetTeamColor(teams[i].teamName),TFT_BLACK);
